4fcaa4f5340e518f70ee7a9be4fbf75d10c57fb0
Natasha Nicole Navarro Nolasco <nn24001@esfe.agape.edu.sv>
2024-03-15 18:29:06 -0600

    Agregar archivos de proyecto.

---
+++ Models/Empleado.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace NATA20241103DB.Models
+{
+    public class Empleado
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+        public string Puesto { get; set; }
+
+        public ICollection<ReferenciaPersonal> ReferenciasPersonales { get; set; }
+    }
+    public class ReferenciaPersonal
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Telefono { get; set; }
+        public string CorreoElectronico { get; set; }
+
+        public int EmpleadoID { get; set; }
+        public Empleado Empleado { get; set; }
+    }
+    public class MiContexto : DbContext
+    {
+        public DbSet<Empleado> Empleados { get; set; }
+        public DbSet<ReferenciaPersonal> ReferenciasPersonales { get; set; }
+    }
+
+}
+
---
+++ Models/NATA20241103DBContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace NATA20241103DB.Models
+{
+    public partial class NATA20241103DBContext : DbContext
+    {
+        public NATA20241103DBContext()
+        {
+        }
+
+        public NATA20241103DBContext(DbContextOptions<NATA20241103DBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Empleado> Empleados { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("server=DESKTOP-OAO2BUA; database=NATA20241103DB; integrated security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Empleado>(entity =>
+            {
+                entity.ToTable("Empleado");
+
+                entity.Property(e => e.ID).ValueGeneratedNever();
+
+                entity.Property(e => e.Apellido).HasMaxLength(100);
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.Nombre).HasMaxLength(100);
+
+                entity.Property(e => e.Puesto).HasMaxLength(100);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
